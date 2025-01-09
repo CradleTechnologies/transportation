9995100e054a54a019d8c7e84f0fd50358a4e01c
Kelvin Sankale <kelvin.sankale@strathmore.edu>
2025-01-09 16:40:01 +0300

    Updates as of 09/January2025

--- src/main/java/com/cradletechnologies/transportation/model/ClientsList_Report.java
+++ src/main/java/com/cradletechnologies/transportation/model/ClientsList_Report.java
@@ -1,16 +1,49 @@
 package com.cradletechnologies.transportation.model;
 
 public class ClientsList_Report {
-	
+
 	private int id;
 	private String firstName;
 	private String lastName;
 	private String clientName;
+	private Double transport;
+	private Double payment;
+	private Double cashIn;
+	private Double cashOut;
 	private String status;
 	private Double balance;
 
 	public ClientsList_Report() {
 
+	}
+
+	public ClientsList_Report(int id, String firstName, String lastName, String clientName, Double transport,
+			Double payment, Double cashIn, Double cashOut, String status, Double balance) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.clientName = clientName;
+		this.transport = transport;
+		this.payment = payment;
+		this.cashIn = cashIn;
+		this.cashOut = cashOut;
+		this.status = status;
+		this.balance = balance;
+	}
+
+	public ClientsList_Report(String firstName, String lastName, String clientName, Double transport, Double payment,
+			Double cashIn, Double cashOut, String status, Double balance) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.clientName = clientName;
+		this.transport = transport;
+		this.payment = payment;
+		this.cashIn = cashIn;
+		this.cashOut = cashOut;
+		this.status = status;
+		this.balance = balance;
 	}
 
 	public ClientsList_Report(int id, String firstName, String lastName, String clientName, String status,
@@ -19,6 +52,14 @@ public class ClientsList_Report {
 		this.id = id;
 		this.firstName = firstName;
 		this.lastName = lastName;
+		this.clientName = clientName;
+		this.status = status;
+		this.balance = balance;
+	}
+
+	public ClientsList_Report(int id, String clientName, String status, Double balance) {
+		super();
+		this.id = id;
 		this.clientName = clientName;
 		this.status = status;
 		this.balance = balance;
@@ -56,6 +97,38 @@ public class ClientsList_Report {
 		this.clientName = clientName;
 	}
 
+	public Double getTransport() {
+		return transport;
+	}
+
+	public void setTransport(Double transport) {
+		this.transport = transport;
+	}
+
+	public Double getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Double payment) {
+		this.payment = payment;
+	}
+
+	public Double getCashIn() {
+		return cashIn;
+	}
+
+	public void setCashIn(Double cashIn) {
+		this.cashIn = cashIn;
+	}
+
+	public Double getCashOut() {
+		return cashOut;
+	}
+
+	public void setCashOut(Double cashOut) {
+		this.cashOut = cashOut;
+	}
+
 	public String getStatus() {
 		return status;
 	}
@@ -71,6 +144,10 @@ public class ClientsList_Report {
 	public void setBalance(Double balance) {
 		this.balance = balance;
 	}
-		
-	
+
+	@Override
+	public String toString() {
+		return "ClientsList_Report [id=" + id + ", clientName=" + clientName + ", balance=" + balance + "]";
+	}
+
 }
